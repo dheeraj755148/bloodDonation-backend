d8bc247104d6efc5381765581c2ff37ce7d2665f
dheeraj755148 <dheerajdayakaran95@gmail.com>
2023-08-26 18:44:44 +0530

    first

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config() // Load environment variables from .env file
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+const morgan = require('morgan')
+
+const mongoose = require('mongoose')
+
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+const port = 9000
+
+mongoose.connect(process.env.DB_URL_NET, {}, (err) => {
+  if (err) {
+    console.log(err.message)
+    throw err
+  }
+  console.log(`mongodb server started...`)
+})
+
+app.use(routes)
+
+app.listen(process.env.PORT, () => console.log(`Listening at: ${port}`))
+
+module.exports = app
